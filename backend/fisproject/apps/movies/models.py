c9d46b8cb090eda1d796a97737d7e4cebcc4001c
Juferoga <juancholoco2000.61@gmail.com>
2023-05-24 16:15:10 -0500

    JR - [ADD] models

--- backend/fisproject/apps/movies/models.py
+++ backend/fisproject/apps/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movies(models.Model):
+  pk_id      = models.AutoField(primary_key=True)
+  b_adult    = models.BooleanField(verbose_name='Adultos',default=False)
+  t_actors   = models.CharField(max_length=30,verbose_name="Actores",blank='')
+  t_overview = models.CharField(max_length=30,verbose_name="Sinopsis",blank='')
+  t_title    = models.CharField(max_length=30,verbose_name='Titulo', blank="")
+  n_rating   = models.IntegerField(verbose_name='Calificacion')
+  t_genre    = models.CharField(max_length=30,verbose_name="Genero", blank='No aplica')
+
+
